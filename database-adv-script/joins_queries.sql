b7cd4f694412d85cf328560c1cd9ebfedf36fc1f
Faithy.dev <fathiaolusegun@gmail.com>
2025-07-13 04:14:28 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,10 +6,15 @@ INNER JOIN users ON bookings.user_id = users.id;
 
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties with no reviews
-SELECT properties.id AS property_id, properties.name AS property_name, reviews.id AS review_id,
-       reviews.comment, reviews.rating
+SELECT 
+  properties.id AS property_id,
+  properties.name AS property_name,
+  reviews.id AS review_id,
+  reviews.comment,
+  reviews.rating
 FROM properties
 LEFT JOIN reviews ON properties.id = reviews.property_id;
+
 
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or booking is not linked to a user
